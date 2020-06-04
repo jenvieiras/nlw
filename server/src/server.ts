0d2396b271836dc474030505702018586c3ea8c6
jenvieiras <jennifervsouza@icloud.com>
2020-06-04 01:42:27 -0300

    conexão com db

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,53 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
+
+/*
+const users = [
+    'Ana',
+    'Maria',
+    'John',
+    'Caleb' 
+];
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
 app.get('/users', (request, response) => {
     console.log("Listagem de Usuários");
 
-    response.json([
-        'Ana',
-        'Maria',
-        'John',
-        'Caleb'
-    ]);
+    return response.json(users);
 });
- 
-app.listen(3333);
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users',  (request, response) => {
+    const data = request.body;
+
+    //console.log(data);
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
+});
+*/ 
